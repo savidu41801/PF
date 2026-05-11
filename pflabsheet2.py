cc4aae7053466da3546725143d72842cc85535fe
savidu41801 <dlsssandeepa@students.nsbm.ac.lk>
2026-05-11 13:26:32 +0530

    Create pflabsheet2.py
    Added PF folder and lab code

---
+++ pflabsheet2.py
@@ -0,0 +1,44 @@
+Name = "Savidu Sathsara"
+School = "Ananda Sastralaya National School "
+
+x = 5 #single line comment
+""""this is multi line comment
+this is a multi line comment"""
+#print(x)
+
+#we using comments for readability, maintenance, collaboration and debugging...
+
+
+result = 22/7
+"""
+print("22 divided by 7 is:", result)
+
+#concatenation means joining two or more strings together to create a new, longer string.
+
+"""
+"""
+operation = "sum"
+total = 8
+print(operation + " is:" + str(total))
+"""
+
+"""
+length = 10
+width = 5
+area = length*width
+print("The area of a rectangle with length " + str(length) + " and width "
+      + str(width) + " is " + str(area))
+"""
+"""
+name = input("Enter your name:")
+
+print("Hello" + name + ", welcome to NSBM!")
+"""
+"""
+print(type(Name))
+print(type(School))
+"""
+
+DoY = input("Enter you Birth Year:")
+DoY = int(DoY)
+print("Your Birth Year is: " + str(DoY))
